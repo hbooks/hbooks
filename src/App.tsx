122a5728457bab4a4f2050c932be2b4d8d4ddfdf
1806 <h00manpublishers@gmail.com>
2026-03-10 20:41:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import Terms from "./pages/Terms";
 import Contact from "./pages/Contact";
 import Support from "./pages/Support";
 import Library from "./pages/Library";
+import ThankYou from "./pages/ThankYou";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -41,6 +42,7 @@ const App = () => {
   <Route path="/support" element={<Support />} />
   <Route path="/library" element={<Library />} />
   {/* New routes */}
+  <Route path="/thankyoupage" element={<ThankYou />} />
   <Route path="/privacy" element={<Privacy />} />
   <Route path="/terms" element={<Terms />} />
   <Route path="*" element={<NotFound />} />
