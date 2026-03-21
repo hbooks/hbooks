72a43170e169c7652f0948cf73df44a8a52ceed1
1806 <h00manpublishers@gmail.com>
2026-03-21 19:01:33 +0300

    Remove QueryClientProvider from App component
    Removed QueryClientProvider from the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -66,7 +65,6 @@ const App = () => {
           </Layout>
         </BrowserRouter>
       </TooltipProvider>
-    </QueryClientProvider>
   );
 };
 
