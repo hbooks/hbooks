217581d26da76ddd029cdaf9bdcf238aa6f8702b
1806 <h00manpublisher@gmail.com>
2026-03-22 17:43:06 +0300

    Switch

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xwomtgvefbshvzgddnig.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3b210Z3ZlZmJzaHZ6Z2RkbmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNzY0OTEsImV4cCI6MjA4NzY1MjQ5MX0.0NidxXSgT7entHMHt-ot1i5EQOXiJWK3aJnW3NRtCV0';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
