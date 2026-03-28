09ab06f9c3f23209593d51ef13d74a275cbfa1ae
1806 <h00manpublishers@gmail.com>
2026-03-28 23:50:47 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,10 +23,11 @@ import PostToDiscord from './pages/PostToDiscord';
 import FAQ from "./pages/FAQ";
 import Helpline from "./pages/Helpline";
 import NotFound from "./pages/NotFound";
+import { AuthProvider } from './contexts/AuthContext';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
-  const isAdminRoute = location.pathname === '/ir806x40';
+  const isAdminRoute = location.pathname === '/ir806x40' || location.pathname === '/pt005Dcd';
 
   return (
     <>
@@ -39,35 +40,37 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
 const App = () => {
   return (
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/books" element={<Books />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/reviews" element={<Reviews />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/support" element={<Support />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="/nonDiscordForm" element={<NonDiscordForm />} />
-            <Route path="/ir806x40" element={<AdminPage />} />
-            <Route path="/stores" element={<Stores />} />
-            <Route path="/pt005Dcd" element={<PostToDiscord />} />
-            <Route path="/thankyoupage" element={<ThankYou />} />
-            <Route path="/privacy" element={<Privacy />} />
-            <Route path="/terms" element={<Terms />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/helpline" element={<Helpline />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-        <AutoReload />
-      </BrowserRouter>
-    </TooltipProvider>
+    <AuthProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/books" element={<Books />} />
+              <Route path="/news" element={<News />} />
+              <Route path="/reviews" element={<Reviews />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/support" element={<Support />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="/nonDiscordForm" element={<NonDiscordForm />} />
+              <Route path="/ir806x40" element={<AdminPage />} />
+              <Route path="/stores" element={<Stores />} />
+              <Route path="/pt005Dcd" element={<PostToDiscord />} />
+              <Route path="/thankyoupage" element={<ThankYou />} />
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="/terms" element={<Terms />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/helpline" element={<Helpline />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+          <AutoReload />
+        </BrowserRouter>
+      </TooltipProvider>
+    </AuthProvider>
   );
 };
 
